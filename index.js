10bcb61678e552a04c37dc84f0abcae77c1678f3
diegopuente <diegoppuente@gmail.com>
2024-08-27 21:30:54 -0300

    variaveis boss

--- index.js
+++
@@ -1,2 +0,0 @@
-let nome heroi = "Diego"
-let xpdebatalha = 2500
---
+++ src/index.js
@@ -0,0 +1,7 @@
+let nomeheroi = "Diego"
+let Boss1 = 800;
+let Boss2 = 950;
+let Boss3 = 1200;
+let xpbatalha = (Boss1 + Boss2 + Boss3);
+
+console.log(xpbatalha);
